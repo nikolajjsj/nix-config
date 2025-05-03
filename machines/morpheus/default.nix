44749d28466744ef781fc30294b5b4a3867e082a
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2025-05-03 21:50:13 +0200

    refactor(morpheus): remove unused system package

--- machines/morpheus/default.nix
+++ machines/morpheus/default.nix
@@ -106,7 +106,6 @@
       libva-vdpau-driver # Previously vaapiVdpau
       intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
       vpl-gpu-rt # QSV on 11th gen or newer
-      intel-ocl # OpenCL support
     ];
   };
 
