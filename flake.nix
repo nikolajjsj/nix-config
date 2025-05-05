67d49a0495167c160352f7de7f47a66c4e881a61
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2025-05-05 14:07:08 +0200

    feat: add home-manager

--- flake.nix
+++ flake.nix
@@ -7,9 +7,13 @@
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, home-manager, nixpkgs }:
     let
       lib = nixpkgs.lib;
     in
