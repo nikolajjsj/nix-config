369f8de35e3595ee9a2135b767945780b0d82200
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2025-05-06 19:59:09 +0200

    fix(morpheus): remove non home-manager params

--- machines/morpheus/home.nix
+++ machines/morpheus/home.nix
@@ -7,17 +7,8 @@
         ../../modules/neovim
         ../../modules/zsh
       ];
-
-      isNormalUser = true;
-      extraGroups = [ "wheel" ];
-      shell = pkgs.zsh;
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF0dkeI+7IdQujtZ3UCSfYB2uPFKZz3i7hWlO4O/sMh+ me@nikolajjsj.com"
-      ];
-
       home = {
         stateVersion = "24.11";
-
         username = "${user}";
         homeDirectory = "/home/${user}";
         packages = [ ];
