77e5b2756b11803019413463bca6895b57875e10
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2025-05-28 08:09:47 +0200

    feat(homelab): remove commented code

--- homelab/services/default.nix
+++ homelab/services/default.nix
@@ -1,34 +1,10 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, ... }:
 {
   options.homelab.services = {
     enable = lib.mkEnableOption "Settings and services for the homelab";
   };
 
   config = lib.mkIf config.homelab.services.enable {
-    # networking.firewall.allowedTCPPorts = [
-    #   80
-    #   443
-    # ];
-    # services.caddy = {
-    #   enable = true;
-    #   globalConfig = ''
-    #     auto_https off
-    #   '';
-    #   virtualHosts = {
-    #     "http://${config.homelab.baseDomain}" = {
-    #       extraConfig = ''
-    #         redir https://{host}{uri}
-    #       '';
-    #     };
-    #     "http://*.${config.homelab.baseDomain}" = {
-    #       extraConfig = ''
-    #         redir https://{host}{uri}
-    #       '';
-    #     };
-    #
-    #   };
-    # };
-
     imports = [
       ./arr.nix
       ./audiobookshelf.nix
